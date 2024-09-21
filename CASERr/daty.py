dd29fb4799524daf2d4fbc2ece904874226c58b2
Barham <154244646+Barham-cayber@users.noreply.github.com>
2024-09-21 13:33:28 +0300

    Add files via upload

---
+++ CASERr/daty.py
@@ -0,0 +1,98 @@
+import asyncio
+from pytgcalls import idle
+import os
+import sys
+import random
+import asyncio
+import redis, re
+from pyrogram import Client
+from pytgcalls import PyTgCalls
+from config import user, dev, call, logger, logger_mode, botname, appp
+from bot import bot_id
+
+API_ID = int("27442758")
+API_HASH = "8288c5c54f568ee346b3227271700b52"
+
+# Get ur redis url from https://app.redislabs.com/
+r = redis.from_url(
+    'redis://default:VL9ITtuHp4uy7dJclHunfu7mBcix1jbT@redis-12071.c93.us-east-1-3.ec2.redns.redis-cloud.com:12071')
+
+def add_Bots(bots):
+    if is_Bots(bots):
+        return
+    r.sadd(f"maker{bot_id}", str(bots))
+
+def is_Bots(bots):
+    try:
+        a = get_Bots()
+        if bots in a:
+            return True
+        return False
+    except:
+        return False
+
+def del_Bots(bots):
+    if not is_Bots(bots):
+        return False
+    r.srem(f"maker{bot_id}", str(bots))
+
+def get_Bots():
+    try:
+        lst = []
+        for a in r.smembers(f"maker{bot_id}"):
+            lst.append(eval(a.decode('utf-8')))
+        return lst
+    except:
+        return []
+        
+async def get_dev(bot_username):
+  dev_id = dev.get(bot_username)
+  if not dev_id:
+   for x in get_Bots():
+    if x[0] == bot_username:
+     dev_id = x[1]
+     dev[bot_username] = dev_id
+     return dev_id
+  return dev_id
+
+async def get_logger(bot_username):
+  logg = logger.get(bot_username)
+  if not logg:
+   for x in get_Bots():
+    if x[0] == bot_username:
+     logg = x[4]
+     logger[bot_username] = logg
+     return logg
+  return logg
+  
+async def get_owner_id(client):
+    bot_username = client.me.username
+    for x in get_Bots():
+        if x[0] == bot_username:
+            owner_id = x[1]
+            dev[bot_username] = owner_id
+            return int(owner_id)
+            
+async def get_userbot(bot_username):
+  userbot = user.get(bot_username)
+  if not userbot:
+   for x in get_Bots():
+    if x[0] == bot_username:
+     SESSION = x[3]
+     userbot = Client("CASER", api_id=API_ID, api_hash=API_HASH, session_string=SESSION)
+     user[bot_username] = userbot
+     return userbot
+  return userbot
+
+async def get_call(bot_username):
+  calll = call.get(bot_username)
+  if not calll:
+   for x in get_Bots():
+    if x[0] == bot_username:
+     userbot = await get_userbot(bot_username)
+     calo = PyTgCalls(userbot, cache_duration=100)
+     await calo.start()
+     call[bot_username] = calo
+     return calo
+  return calll  
+  
---
+++ casery.py
@@ -0,0 +1,16 @@
+##مطور السورس
+caes = ["IQ7amo"]
+casery = "IQ7amo"
+##ايدي مطور السورس
+caserid = 5576431780
+##قناه السورس
+source = "https://t.me/MGIMT" 
+ch = "EHS4SS" 
+##جروب السورس
+group = "https://t.me/GroupAlina" 
+##لوجو السورس
+photosource = "https://telegra.ph/file/79d346793ec3e73d5e9f0.jpg"
+##توكن الصانع
+bot_token="7274541283:AAG9Ir5v2coJj2pgtU2-CtSZ_UfGV7hmqvM"
+##جلسه الاشتراك
+bot_token2="AgGCIEoAtEiLue0lw-jCPkfj-0XOry77l0V0AbHgB6IOBdk1P-x5XpnS74vsClfplCUiC6h9DKLEgxWpdH1yDMSc21lM1W0UKS6AHJMsVUMyAyCYzXqNr9svFHLKUkoDrYtB1C5KQ4Q-SEAx0GQ7cnLLZofVLzVSZJo-6s_jH5-gDsZQAXuy6-YmPLnfIcGdTusGQN0Q4SekH0POsxvHgv6tkeaWaVDzkIkPn3RTcJ1SLBt6s70EOPgto7H0_CZlMgEiy07qEvU4JoL-OrcUmhEv1JOULBIlWD0opHy6lNBeOkbs8CPJaxhbBv_R-ev8oT0WsEbIdJP2UwZATuFAibpcPm7ZKwAAAAF7mkr6AA"
